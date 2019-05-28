329e71510516bacc3625b38e6df8bace951e3b34
zhihua <zhihua@uw.edu>
2019-05-27 21:46:28 -0700

    Complete eddy fluxes calculation

--- gfd_hw3.m
+++ gfd_hw3.m
@@ -14,10 +14,9 @@ f0 = 1e-4; % [s^-1]
 H  = [500 500]; % [m]
 g  = 9.81; % gravity [m/s^2]
 gp = 0.1; % reduced gravity [m/s^2]
-kd = 1/100e3; % 1/k_d [m^-1]
+kd = 1/(50e03); % k_d [m^-1]
 
 alpha = gsw_alpha(35,20,0); % [1/degree C]
-% rho0  = 1025; % [kg/m^3]
 
 %% Grid
 
@@ -67,27 +66,31 @@ u     = U-U_bar;
 
 %% 
 
+cmap_div = flipud( cbrewer('div','RdBu',28) );
+
 figure('position',[0 0 600 600])
 
 subplot(2,1,1)
 contourf(Tj,Yj,squeeze(U_bar(:,1,1,:)),6,'LineWidth',.8,'LineColor',[.2 .2 .2])
-cmocean('balance',28)
+colormap(cmap_div);
+% cmocean('balance',28)
 colorbar
-caxis([-1.4 1.4])
+caxis([-1.2 1.2])
 xlabel('time [day]')
 ylabel('cross-jet distance [km]')
 title('Zonally averaged zonal velocity - Layer 1 [m/s]')
 
 subplot(2,1,2)
 contourf(Tj,Yj,squeeze(U_bar(:,1,2,:)),6,'LineWidth',.8,'LineColor',[.2 .2 .2])
-cmocean('balance',28)
+colormap(cmap_div);
+% cmocean('balance',28)
 colorbar
-caxis([-1.4 1.4])
+caxis([-1.2 1.2])
 xlabel('time [day]')
 ylabel('cross-jet distance [km]')
 title('Zonally averaged zonal velocity - Layer 2 [m/s]')
 
-% saveas(gcf,'./figs/hw3/Ubar','png')
+saveas(gcf,'./figs/hw3/Ubar','png')
 
 %% Zonal average of interface height
 
@@ -98,15 +101,20 @@ eta_bar = trapz(x,eta,2)/Lx;
 
 %%
 
+cmap_div = flipud( cbrewer('div','RdBu',30) );
+
 figure('position',[0 0 600 300])
 
 contourf(T,Y,squeeze(eta_bar),10,'LineWidth',.8,'LineColor',[.2 .2 .2])
-cmocean('balance',28)
+colormap(cmap_div);
+% cmocean('balance',28)
 colorbar
-caxis([-140 140])
+caxis([-120 120])
 xlabel('time [day]')
 ylabel('cross-jet distance [km]')
 title('Zonally averaged interface height [m]')
+
+saveas(gcf,'./figs/hw3/eta_bar','png')
 
 %% Zonal average of potential vorticity
 
@@ -127,25 +135,31 @@ q_p   = q - q_bar;
 
 %%
 
+% cmap_div = flipud( cbrewer('div','BrBG',20) );
+
 figure('position',[0 0 600 600])
 
 subplot(2,1,1)
-contourf(Tjj,Yjj,squeeze(q_bar(:,1,1,:)),10,'LineStyle','none')
-cmocean('curl',20)
+contourf(Tjj,Yjj,squeeze(q_bar(:,1,1,:)),20,'LineStyle','none')
+% colormap(cmap_div);
+cmocean('curl',50)
 colorbar
-caxis([-3.1e-5 3.1e-5])
+caxis([-4.8e-5 4.8e-5])
 xlabel('time [day]')
 ylabel('cross-jet distance [km]')
 title('Zonally averaged potential vorticity - Layer 1 [1/s]')
 
 subplot(2,1,2)
-contourf(Tjj,Yjj,squeeze(q_bar(:,1,2,:)),10,'LineStyle','none')
-cmocean('curl',20)
+contourf(Tjj,Yjj,squeeze(q_bar(:,1,2,:)),20,'LineStyle','none')
+% colormap(cmap_div);
+cmocean('curl',50)
 colorbar
-caxis([-3.1e-5 3.1e-5])
+caxis([-4.8e-5 4.8e-5])
 xlabel('time [day]')
 ylabel('cross-jet distance [km]')
 title('Zonally averaged potential vorticity - Layer 2 [1/s]')
+
+saveas(gcf,'./figs/hw3/q_bar','png')
 
 %% Zonal average of potential vorticity gradient
 
@@ -161,25 +175,31 @@ dq_dy_bar = squeeze(trapz(xii,dq_dy,2))/Lxii;
 
 %%
 
+% cmap_div = flipud( cbrewer('div','RdBu',32) );
+
 figure('position',[0 0 600 600])
 
 subplot(2,1,1)
-contourf(Tjjj,Yjjj,squeeze(dq_dy_bar(:,1,:)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
-cmocean('balance',28)
+contourf(Tjjj,Yjjj,squeeze(dq_dy_bar(:,1,:)),20,'LineStyle','none')
+% colormap(cmap_div);
+cmocean('curl',28)
 colorbar
 caxis([-1.4e-9 1.4e-9])
 xlabel('time [day]')
 ylabel('cross-jet distance [km]')
-title('Zonally averaged potential vorticity gradient - Layer 1 [m^{-1}s^{-1}]')
+title('Zonally averaged PV gradient Q_y - Layer 1 [m^{-1}s^{-1}]')
 
 subplot(2,1,2)
-contourf(Tjjj,Yjjj,squeeze(dq_dy_bar(:,2,:)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
-cmocean('balance',28)
+contourf(Tjjj,Yjjj,squeeze(dq_dy_bar(:,2,:)),20,'LineStyle','none')
+% colormap(cmap_div);
+cmocean('curl',28)
 colorbar
 caxis([-1.4e-9 1.4e-9])
 xlabel('time [day]')
 ylabel('cross-jet distance [km]')
-title('Zonally averaged potential vorticity gradient - Layer 2 [m^{-1}s^{-1}]')
+title('Zonally averaged PV gradient Q_y - Layer 2 [m^{-1}s^{-1}]')
+
+saveas(gcf,'./figs/hw3/dqdy_bar','png')
 
 %% Perturbation streamfunction
 
@@ -191,52 +211,61 @@ PSI_p = PSI - PSI_bar;
 
 t_ind(1) = find(t==1);
 t_ind(2) = find(t==6);
+t_ind(3) = find(t==5);
 
 [X,Y] = meshgrid(x,y);
 
 %%
 
+cmap_div = flipud( cbrewer('div','PiYG',28) );
+
 figure('position',[0 0 750 600])
 
 subplot(2,2,1)
-contourf(X,Y,PSI_p(:,:,1,t_ind(1)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
-cmocean('balance',22)
+contourf(X,Y,PSI_p(:,:,1,t_ind(1)),10,'LineWidth',.8,'LineColor',[.2 .2 .2])
+colormap(cmap_div);
+% cmocean('balance',22)
 colorbar
-caxis([-1.1e4 1.1e4])
+caxis([-1.15e4 1.15e4])
 xlabel('along-jet distance [km]')
 ylabel('cross-jet distance [km]')
 legend('day 1')
 title('Perturbation streamfunction - Layer 1 [m^{2}s^{-1}]')
 
 subplot(2,2,2)
-contourf(X,Y,PSI_p(:,:,2,t_ind(1)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
-cmocean('balance',22)
+contourf(X,Y,PSI_p(:,:,1,t_ind(2)),10,'LineWidth',.8,'LineColor',[.2 .2 .2])
+colormap(cmap_div);
+% cmocean('balance',22)
 colorbar
-caxis([-1.1e4 1.1e4])
-xlabel('along-jet distance [km]')
-ylabel('cross-jet distance [km]')
-legend('day 1')
-title('Perturbation streamfunction - Layer 2 [m^{2}s^{-1}]')
-
-subplot(2,2,3)
-contourf(X,Y,PSI_p(:,:,1,t_ind(2)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
-cmocean('balance',22)
-colorbar
-caxis([-1.1e4 1.1e4])
+caxis([-1.15e4 1.15e4])
 xlabel('along-jet distance [km]')
 ylabel('cross-jet distance [km]')
 legend('day 6')
 title('Perturbation streamfunction - Layer 1 [m^{2}s^{-1}]')
 
-subplot(2,2,4)
-contourf(X,Y,PSI_p(:,:,2,t_ind(2)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
-cmocean('balance',22)
+subplot(2,2,3)
+contourf(X,Y,PSI_p(:,:,2,t_ind(1)),10,'LineWidth',.8,'LineColor',[.2 .2 .2])
+colormap(cmap_div);
+% cmocean('balance',22)
 colorbar
-caxis([-1.1e4 1.1e4])
+caxis([-1.15e4 1.15e4])
+xlabel('along-jet distance [km]')
+ylabel('cross-jet distance [km]')
+legend('day 1')
+title('Perturbation streamfunction - Layer 2 [m^{2}s^{-1}]')
+
+subplot(2,2,4)
+contourf(X,Y,PSI_p(:,:,2,t_ind(2)),10,'LineWidth',.8,'LineColor',[.2 .2 .2])
+colormap(cmap_div);
+% cmocean('balance',22)
+colorbar
+caxis([-1.15e4 1.15e4])
 xlabel('along-jet distance [km]')
 ylabel('cross-jet distance [km]')
 legend('day 6')
 title('Perturbation streamfunction - Layer 2 [m^{2}s^{-1}]')
+
+saveas(gcf,'./figs/hw3/psi_p','png')
 
 %% Perturbation temperature and meridional velocity
 
@@ -252,8 +281,6 @@ v(:,:,2,:) = center_diff(PSI_p(:,:,2,:),1000*x,2);
 
 v_eta   = squeeze(mean(v,3));
 
-[Xi,Yi] = meshgrid(xi,y);
-
 %% 
 
 % figure('position',[0 0 460 340])
@@ -266,19 +293,25 @@ v_eta   = squeeze(mean(v,3));
 % legend('day 1')
 % title('Meridional velocity at interface [m/s]')
 
-figure('position',[0 0 500 240])
+figure('position',[0 0 550 260])
 xlabel('along-jet distance [km]')
 yyaxis left
 plot(xi,v_eta(ny/2,:,t_ind(1)),'linewidth',1.5)
 ylabel('meridional velocity at interface [m/s]')
 yyaxis right
-plot(x,theta_p(ny/2,:,t_ind(1)),'linewidth',1.5)
-ylabel('temperature perturbation ')
+plot(x,eta_p(ny/2,:,t_ind(1)),'linewidth',1.5)
+ylabel('interface height perturbation [m]')
 xlim([x(1) x(end)])
+hold on
+plot([x(1) x(end)],[0 0],'k','linestyle',':')
+hold off
+
+saveas(gcf,'./figs/hw3/etap_vs_v','png')
 
 %% movie
 
 [X,Y] = meshgrid(x,y);
+[Xi,Yi] = meshgrid(xi,y);
 [Xj,Yj] = meshgrid(x,yj);
 [Xiijj,Yiijj] = meshgrid(xii,yjj);
 
@@ -286,8 +319,9 @@ figure('position',[0 0 460 340])
 
 for i=1:nt
     
-    pcolor(X,Y,eta(:,:,1,i));
+    pcolor(Xiijj,Yiijj,q(:,:,i));
     shading interp
+    colorbar
     text(120,300,['day ',num2str(t(i))]);
     pause(0.1)
 end
@@ -302,25 +336,31 @@ vq_p_bar = trapz(xii,vq_p,2)/Lxii; % <v'q'> [m/s^2]
 
 %%
 
+% cmap_div = flipud( cbrewer('div','RdBu',26) );
+
 figure('position',[0 0 600 600])
 
 subplot(2,1,1)
 contourf(Tjj,Yjj,squeeze(vq_p_bar(:,1,1,:)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
-cmocean('balance',26)
+% colormap(cmap_div);
+cmocean('curl',36)
 colorbar
-caxis([-1.3e-5 1.3e-5])
+caxis([-1.8e-5 1.8e-5])
 xlabel('time [day]')
 ylabel('cross-jet distance [km]')
 title('Zonally averaged meridional PV flux - Layer 1 [m*s^{-2}]')
 
 subplot(2,1,2)
 contourf(Tjj,Yjj,squeeze(vq_p_bar(:,1,2,:)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
-cmocean('balance',26)
+% colormap(cmap_div);
+cmocean('curl',36)
 colorbar
-caxis([-1.3e-5 1.3e-5])
+caxis([-1.8e-5 1.8e-5])
 xlabel('time [day]')
 ylabel('cross-jet distance [km]')
 title('Zonally averaged meridional PV flux - Layer 2 [m*s^{-2}]')
+
+saveas(gcf,'./figs/hw3/q_flux','png')
 
 %% Zonal average of meridional momentum flux
 
@@ -329,53 +369,110 @@ ui   = (u(:,1:end-1,:,:) + u(:,2:end,:,:))/2; % project u to xi grid
 
 vu_p = vj .* ui; % v'u' [m^2/s^2]
 
-% Zonal average
+% zonal average
 vu_p_bar = trapz(xi,vu_p,2)/Lxi;
 
-[Tj,Yj] = meshgrid(t,yj);
+duflux_dy(:,:,1,:) = center_diff(vu_p_bar(:,:,1,:),1000*yj,1);
+duflux_dy(:,:,2,:) = center_diff(vu_p_bar(:,:,2,:),1000*yj,1);
+
+[Tj,Yj]   = meshgrid(t,yj);
+[Tjj,Yjj] = meshgrid(t,yjj);
+
+%%
+
+cmap_div = flipud( cbrewer('div','RdBu',32) );
+
+figure('position',[0 0 800 400])
+
+subplot(2,2,1)
+contourf(Tj,Yj,squeeze(vu_p_bar(:,1,1,:)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
+colormap(cmap_div)
+% cmocean('balance',34)
+colorbar
+caxis([-1.6e-1 1.6e-1])
+xlabel('time [day]')
+ylabel('cross-jet distance [km]')
+title('momentum flux - Layer 1 [m^{2}*s^{-2}]')
+
+subplot(2,2,2)
+contourf(Tjj,Yjj,squeeze(duflux_dy(:,1,1,:)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
+colormap(cmap_div)
+colorbar
+caxis([-7.4e-6 7.4e-6])
+xlabel('time [day]')
+ylabel('cross-jet distance [km]')
+title('momentum flux gradient - Layer 1 [m*s^{-2}]')
+
+subplot(2,2,3)
+contourf(Tj,Yj,squeeze(vu_p_bar(:,1,2,:)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
+colormap(cmap_div)
+% cmocean('balance',34)
+colorbar
+caxis([-1.6e-1 1.6e-1])
+xlabel('time [day]')
+ylabel('cross-jet distance [km]')
+title('momentum flux - Layer 2 [m^{2}*s^{-2}]')
+
+subplot(2,2,4)
+contourf(Tjj,Yjj,squeeze(duflux_dy(:,1,2,:)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
+colormap(cmap_div)
+colorbar
+caxis([-7.4e-6 7.4e-6])
+xlabel('time [day]')
+ylabel('cross-jet distance [km]')
+title('momentum flux gradient - Layer 2 [m*s^{-2}]')
+
+saveas(gcf,'./figs/hw3/u_flux','png')
 
 %%
 
 figure('position',[0 0 600 600])
 
 subplot(2,1,1)
-contourf(Tj,Yj,squeeze(vu_p_bar(:,1,1,:)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
-cmocean('balance',34)
+contourf(Tjj,Yjj,squeeze(duflux_dy(:,1,1,:)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
+colormap(cmap_div)
 colorbar
-caxis([-1.7e-1 1.7e-1])
+caxis([-7.4e-6 7.4e-6])
 xlabel('time [day]')
 ylabel('cross-jet distance [km]')
-title('Zonally averaged meridional momentum flux - Layer 1 [m^{2}*s^{-2}]')
+title('meridional gradient of meridional momentum flux - Layer 1 [m*s^{-2}]')
 
 subplot(2,1,2)
-contourf(Tj,Yj,squeeze(vu_p_bar(:,1,2,:)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
-cmocean('balance',34)
+contourf(Tjj,Yjj,squeeze(duflux_dy(:,1,2,:)),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
+colormap(cmap_div)
 colorbar
-caxis([-1.7e-1 1.7e-1])
+caxis([-7.4e-6 7.4e-6])
 xlabel('time [day]')
 ylabel('cross-jet distance [km]')
-title('Zonally averaged meridional momentum flux - Layer 2 [m^{2}*s^{-2}]')
+title('meridional gradient of meridional momentum flux - Layer 2 [m*s^{-2}]')
 
-%% Zonal average of meridional momentum flux
+
+%% Zonal average of meridional interface height flux
 
 eta_pi = (eta_p(:,1:end-1,:,:) + eta_p(:,2:end,:,:))/2; % project eta_p to xi grid
 veta_p = v_eta .* eta_pi; % v_eta'\eta' [m^2/s]
 
-% Zonal average
+% zonal average
 veta_p_bar = trapz(xi,veta_p,2)/Lxi;
 
 [T,Y] = meshgrid(t,y);
 
 %%
 
+cmap_div = flipud( cbrewer('div','RdBu',50) );
+
 figure('position',[0 0 600 300])
 
 contourf(T,Y,squeeze(veta_p_bar),8,'LineWidth',.8,'LineColor',[.2 .2 .2])
-cmocean('balance',24)
+colormap(cmap_div)
+% cmocean('balance',24)
 colorbar
-caxis([-6e1 6e1])
+caxis([-5e1 5e1])
 xlabel('time [day]')
 ylabel('cross-jet distance [km]')
 title('Zonally averaged meridional interface height flux [m^{2}/s]')
 
-%%
+saveas(gcf,'./figs/hw3/eta_flux','png')
+
+%% 
+
